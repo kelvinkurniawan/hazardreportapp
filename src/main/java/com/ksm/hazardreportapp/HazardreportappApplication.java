aa91fc206f6a29c0bc3f9e88404faaf99c23be2a
Kelvin Kurniawan O <tepinnko@gmail.com>
2020-11-25 18:01:05 +0700

    Initial commit - boilerplate

---
+++ src/main/java/com/ksm/hazardreportapp/HazardreportappApplication.java
@@ -0,0 +1,13 @@
+package com.ksm.hazardreportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazardreportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazardreportappApplication.class, args);
+	}
+
+}
